fe6c85d5f91e3f24d1da2a45f4cb5cc417bad1c9
Shivakant6387 <shivakant5875@gmail.com>
2023-07-04 11:49:40 +0530

    is blank method it is return true or false

---
+++ src/main/java/org/example/IsBlankTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class IsBlankTest {
+    public static void main(String[] args) {
+        String str=" ";
+        String str2=" ";
+        String str3="Nothing to work";
+        System.out.println(str+"str is blank :"+str.isBlank());
+        System.out.println(str2+"str2 is blank :"+str2.isBlank());
+        System.out.println(str3+"str3 is blank  :"+str3.isBlank());
+    }
+}
