5b9ad7f12651c2950384dad74654503260830479
Shivakant6387 <shivakant5875@gmail.com>
2023-07-04 11:56:06 +0530

    repeat of the string

---
+++ src/main/java/org/example/Rpeat.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Rpeat {
+    public static void main(String[] args) {
+        String str="String";
+        System.out.println("String :"+str);
+        System.out.println("String repeated twice :"+str.repeat(2));
+        System.out.println("String repeated five times :"+str.repeat(5));
+    }
+}
